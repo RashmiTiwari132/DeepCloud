a584e4334533966d8fbe8a4fe8b630589fb0a7ba
RashmiTiwari132 <trashmi12@gmail.com>
2016-11-21 16:30:59 -0500

    added delete feature and renamed createDeployment from createdeployment

--- Kubernetes/DockerBackendInterface.js
+++ Kubernetes/DockerBackendInterface.js
@@ -5,7 +5,11 @@ var deploy_application = function(userId){
 	ODeployContainers.create_deployment(userId);
 };
 
-module.exports.deploy_application = deploy_application;
+//module.exports.deploy_application = deploy_application;
+module.exports = {
+	
+	
+}
 
 
 
